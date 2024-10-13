83f801f5f52bbccfad3e6d51c1cee32ab9427764
Maia Grotepass <maiatoday@gmail.com>
2024-10-13 23:30:31 +0200

    more rhymes

--- util/src/main/kotlin/Rhymes.kt
+++ util/src/main/kotlin/Rhymes.kt
@@ -55,3 +55,118 @@ val oneTwo = mapOf(
     18 to "maids in waiting",
     20 to "my plate's empty!",
 )
+
+val tenamalin = listOf(
+    "Onery",
+    "Twoery",
+    "Tickery",
+    "Tin",
+    "Alamacrack",
+    "Tenamalin",
+    "Pin",
+    "Pan",
+    "Musky Dan",
+    "Tweedleum",
+    "Twiddleum",
+    "Twenty-one",
+    "Black fish",
+    "White trout",
+    "Eery, Ory",
+    "You are out.",
+)
+
+val nutTree = listOf(
+    "I had a little nut-tree, nothing would it bear",
+    "But a silver nutmeg and a golden pear;",
+    "The King of Spain's daughter came to visit me,",
+    "And all was because of my little nut-tree.",
+    "I skipp'd over water, I danced over sea,",
+    "And all the birds in the air couldn't catch me.",
+)
+
+val wishes = listOf(
+    "If wishes were horses,",
+    "Beggars would ride;",
+    "If turnips were watches,",
+    "I would wear one by my side.",
+)
+
+val luck = listOf(
+    "See a pin and pick it up,",
+    "All the day you'll have good luck;",
+    "See a pin and let it lay,",
+    "Bad luck you'll have all the day!",
+)
+
+val pie = listOf(
+    "Sing a song of sixpence,",
+    "A bag full of rye;",
+    "Four and twenty blackbirds",
+    "Baked in a pie;",
+    "",
+    "When the pie was open'd,",
+    "The birds began to sing;",
+    "Was not that a dainty dish,",
+    "To set before the king?",
+    "",
+    "The king was in his counting-house",
+    "Counting out his money;",
+    "The queen was in the parlour",
+    "Eating bread and honey;",
+    "",
+    "The maid was in the garden",
+    "Hanging out the clothes,",
+    "There came a little blackbird,",
+    "And snapt off her nose.",
+)
+
+val drink = listOf(
+    "If all the world was apple-pie,",
+    "And all the sea was ink,",
+    "And all the trees were bread and cheese,",
+    "What should we have for drink?",
+)
+
+val riddle = listOf(
+    "The man in the wilderness asked me",
+    "How many strawberries grew in the sea.",
+    "I answered him as I thought good,",
+    "As many as red herrings grew in the wood.",
+)
+
+val play = listOf(
+    "Girls and boys, come out to play;",
+    "The moon doth shine as bright as day;",
+    "Leave your supper, and leave your sleep,",
+    "And come with your playfellows into the street.",
+    "Come with a whoop, come with a call,",
+    "Come with a good will or not at all.",
+    "Up the ladder and down the wall,",
+    "A halfpenny roll will serve us all.",
+    "You find milk, and I'll find flour,",
+    "And we'll have a pudding in half-an-hour.",
+)
+
+val sea = listOf(
+    "If all the seas were one sea,",
+    "What a great sea that would be!",
+    "And if all the trees were one tree,",
+    "What a great tree that would be!",
+    "And if all the axes were one axe,",
+    "What a great axe that would be!",
+    "And if all the men were one man,",
+    "What a great man he would be!",
+    "And if the great man took the great axe,",
+    "And cut down the great tree,",
+    "And let it fall into the great sea,",
+    "What a splish splash that would be!",
+)
+
+val year = listOf(
+    "Thirty days hath September,",
+    "April, June, and November;",
+    "February has twenty-eight alone,",
+    "All the rest have thirty-one,",
+    "Excepting leap-year, that's the time",
+    "When February's days are twenty-nine.",
+)
