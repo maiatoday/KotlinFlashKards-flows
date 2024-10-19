7fe1d72fed405cdb8e26bd0cdf718382207c5ca3
Maia Grotepass <maiatoday@gmail.com>
2024-10-19 19:18:45 +0200

    Cold Flow 4

--- Flows/Cold flows/4/src/Main.kt
+++ Flows/Cold flows/4/src/Main.kt
@@ -1,3 +1,19 @@
-fun main() {
-    // Write your solution here
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import net.maiatoday.rhymes.tenamalin
+
+val tenamalinRhyme = flow {
+    println("Tenamalin Rhyme starts to emit...")
+    tenamalin.forEach { emit(it) }
+}
+
+fun main() = runBlocking {
+    println("Main: starts running, flow is not emitting yet")
+    println("Main: make a new filtered flow")
+    val tenamalinRhymeEndsWithT = tenamalinRhyme
+        .filter { it.startsWith("T") }
+    println("Main: collect the filtered tenamalin Rhyme flow")
+    tenamalinRhymeEndsWithT.collect { println(it) }
+    println("Main: finished collecting")
 }
