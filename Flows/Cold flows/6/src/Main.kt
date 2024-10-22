aff0853d7e5b30d69cd9751cf03804001fc1971c
Maia Grotepass <maiatoday@gmail.com>
2024-10-22 23:15:08 +0200

    Cold Flow 6

--- Flows/Cold flows/6/src/Main.kt
+++ Flows/Cold flows/6/src/Main.kt
@@ -1,3 +1,21 @@
-fun main() {
-    // Write your solution here
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import net.maiatoday.rhymes.dishSpoon
+import kotlin.time.Duration.Companion.milliseconds
+
+// Click into the Flow interface, the flow builder and the FlowCollector interface
+// to see what the interface looks like.
+val nurseryRhyme: Flow<String> = flow {
+    for (line in dishSpoon) {
+        emit(line)
+        delay(10.milliseconds)
+    }
+}
+
+fun main() = runBlocking {
+    nurseryRhyme.collect {
+        println(it)
+    }
 }
--- Flows/Cold flows/6/task.md
+++ Flows/Cold flows/6/task.md
@@ -1,4 +1,4 @@
-The flow interfaces are really elegant. See the code snippet with a recreation of what they look like. **What language keyword makes it possible to create flows?**
+The flow interfaces are really elegant. They simply consist of a Flow and a flow collector where the collector lambda ia called when the flow emits. See the code snippet and click into the library to see what they look like. **What language keyword makes it possible to create flows?**
 
 <div class="hint">
   suspend
