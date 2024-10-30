4e3dd57dd87c1baa34b5eae7076d72a691ce36b8
Maia Grotepass <maiatoday@gmail.com>
2024-10-30 22:24:47 +0100

    Hot flow 7

--- Flows/Hot flows/7/src/Task.kt
+++ Flows/Hot flows/7/src/Task.kt
@@ -1,1 +1,41 @@
-// type your solution here
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import net.maiatoday.rhymes.tenamalin
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+
+class RhymeReciter {
+    private val _lines = MutableSharedFlow<String> (replay = 5)
+    val lines = _lines.asSharedFlow()
+    private var job:Job? = null
+
+    fun startReciting(scope: CoroutineScope) {
+        job = scope.launch {
+            tenamalin.forEach {
+                _lines.emit(it)
+                delay(100.milliseconds)
+            }
+        }
+    }
+
+    fun stopReciting() {
+        job?.cancel()
+    }
+}
+
+fun main():Unit = runBlocking {
+    val reciter = RhymeReciter()
+    launch {
+        reciter.lines.collect { println("subscriber 1: $it") }
+    }
+    launch {
+        delay(1.seconds) // subscriber 2 misses the beginning
+        println("....subscriber 2 delayed")
+        reciter.lines.collect { println("    subscriber 2: $it") }
+    }
+    reciter.startReciting(this)
+    delay(3.seconds)
+    reciter.stopReciting()
+    println("Done")
+}
--- Flows/Hot flows/7/test/Tests.kt
+++ Flows/Hot flows/7/test/Tests.kt
@@ -1,9 +1,23 @@
-import org.junit.Assert
+import app.cash.turbine.test
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runTest
+import net.maiatoday.rhymes.tenamalin
 import org.junit.Test
+import kotlin.test.DefaultAsserter.assertEquals
+import kotlin.time.Duration.Companion.milliseconds
 
 class Test {
-    @Test fun testSolution() {
-        //TODO: implement your test here
-        Assert.assertTrue("Tests not implemented for the task", false)
+    @Test fun testSolution() = runTest {
+        val reciter = RhymeReciter()
+        reciter.startReciting(this)
+        delay(200.milliseconds) // even though the first two items are emitted
+        // already because of the replay the whole list will be replayed
+        reciter.lines.test {
+            tenamalin.forEach {
+                assertEquals("Tenamalin items not correct", it, awaitItem())
+            }
+            cancelAndConsumeRemainingEvents()
+        }
+        reciter.stopReciting()
     }
 }
