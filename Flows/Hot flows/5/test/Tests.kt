6db4aae584d11430e9457c2eab3a33379b798030
Maia Grotepass <maiatoday@gmail.com>
2024-10-30 22:06:09 +0100

    Hot flow 5

--- Flows/Hot flows/5/test/Tests.kt
+++ Flows/Hot flows/5/test/Tests.kt
@@ -1,9 +1,20 @@
-import org.junit.Assert
+import app.cash.turbine.test
+import kotlinx.coroutines.test.runTest
+import net.maiatoday.rhymes.tenamalin
 import org.junit.Test
+import kotlin.test.DefaultAsserter.assertEquals
 
-class Test {
-    @Test fun testSolution() {
-        //TODO: implement your test here
-        Assert.assertTrue("Tests not implemented for the task", false)
+class Tests {
+    @Test
+    fun testSolution() = runTest {
+        val reciter = RhymeReciter()
+        reciter.startReciting(this)
+        reciter.lines.test {
+            tenamalin.forEach {
+                assertEquals("Tenamalin items not correct", it, awaitItem())
+            }
+            cancelAndConsumeRemainingEvents()
+        }
+        reciter.stopReciting()
     }
 }
