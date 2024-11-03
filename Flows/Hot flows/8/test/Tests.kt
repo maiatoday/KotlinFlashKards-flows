925e0742ff72da86777e1e2fb66d7551deae2b5e
Maia Grotepass <maiatoday@gmail.com>
2024-11-03 19:18:09 +0100

    Hot flow 8

--- Flows/Hot flows/8/test/Tests.kt
+++ Flows/Hot flows/8/test/Tests.kt
@@ -1,9 +1,16 @@
-import org.junit.Assert
+import app.cash.turbine.test
+import kotlinx.coroutines.runBlocking
+import net.maiatoday.rhymes.drink
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class Test {
-    @Test fun testSolution() {
-        //TODO: implement your test here
-        Assert.assertTrue("Tests not implemented for the task", false)
+    @Test fun testSolution() = runBlocking {
+       val sharedFlow  = getSharedFlowFromCold(this, coldFlow)
+        sharedFlow.test {
+            drink.forEach {
+                assertEquals(it, awaitItem())
+            }
+        }
     }
 }
