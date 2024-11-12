c9d5a017f3773bf85888260addfebb6f73d946ef
Maia Grotepass <maiatoday@gmail.com>
2024-11-12 20:30:59 +0100

    Hot flow 15

--- Flows/Hot flows/15/src/Main.kt
+++ Flows/Hot flows/15/src/Main.kt
@@ -1,3 +1,34 @@
-fun main() {
-    // Write your solution here
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import net.maiatoday.rhymes.tenamalin
+import kotlin.time.Duration.Companion.milliseconds
+
+class RhymeLastWord {
+    private val _word = MutableStateFlow("")
+    val word = _word.asStateFlow()
+    private var job: Job? = null
+
+    fun startReciting(scope: CoroutineScope) {
+        job = scope.launch {
+            val doubledList = tenamalin.flatMap { listOf(it, it) }
+
+            doubledList.forEach { word ->
+                println("     updating $word")
+                _word.update { word }
+                delay(100.milliseconds)
+            }
+        }
+    }
+
+    fun stopReciting() {
+        job?.cancel()
+    }
+}
+
+fun main(): Unit = runBlocking {
+    val reciter = RhymeLastWord()
+    reciter.startReciting(this)
+    reciter.word.collect { println(it) }
 }
