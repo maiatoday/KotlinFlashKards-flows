757c4302692bf8167e757f289bdf4c3cd69ec6f3
Maia Grotepass <maiatoday@gmail.com>
2024-11-12 19:59:22 +0100

    Fix tests 11

--- Flows/Hot flows/11/test/Tests.kt
+++ Flows/Hot flows/11/test/Tests.kt
@@ -1,15 +1,18 @@
 import app.cash.turbine.test
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import net.maiatoday.rhymes.play
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class Test {
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun testSolution() = runTest {
-        val sharedFlow = getSharedFlowFromCold(this, coldFlow)
+        val scope = CoroutineScope(UnconfinedTestDispatcher(testScheduler))
+        val sharedFlow = getSharedFlowFromCold(scope, coldFlow)
         sharedFlow.test {
             
             play.forEach {
