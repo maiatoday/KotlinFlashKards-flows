e0d3a6b7cedb154e32d6621d74c5500033ca54a2
Maia Grotepass <maiatoday@gmail.com>
2024-11-12 22:58:06 +0100

    multiple choice 20

--- Flows/Hot flows/20/src/Main.kt
+++ Flows/Hot flows/20/src/Main.kt
@@ -1,3 +1,28 @@
-fun main() {
-    // Write your solution here
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import net.maiatoday.rhymes.basket
+import kotlin.time.Duration.Companion.milliseconds
+
+val  wishFlow: Flow<String> = channelFlow {
+    launch {
+        for (line in basket) {
+            delay(100.milliseconds)
+            send(line)
+        }
+    }
+    launch {
+        for (line in basket) {
+            delay(200.milliseconds)
+            send(line)
+        }
+    }
+}
+
+fun main() = runBlocking {
+    wishFlow.collect {
+        println(it)
+    }
 }
