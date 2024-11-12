5e73f31291f83384d408e984796ccee11bde8bb9
Maia Grotepass <maiatoday@gmail.com>
2024-11-12 22:55:05 +0100

    multiple choice 17, 18, 19

--- Flows/Hot flows/17/src/Main.kt
+++ Flows/Hot flows/17/src/Main.kt
@@ -1,3 +1,20 @@
-fun main() {
-    // Write your solution here
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import net.maiatoday.rhymes.dishSpoon
+import kotlin.time.Duration.Companion.milliseconds
+
+val nurseryRhyme = flow {
+    println("Starting")
+    for (line in dishSpoon) {
+        emit(line)
+        delay(10.milliseconds)
+    }
+}
+
+fun main() = runBlocking {
+    println("Has the nurseryRhyme started?")
+    nurseryRhyme.collect {
+        println(it)
+    }
 }
--- Flows/Hot flows/19/src/Main.kt
+++ Flows/Hot flows/19/src/Main.kt
@@ -1,3 +1,37 @@
-fun main() {
-    // Write your solution here
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import net.maiatoday.rhymes.basket
+import net.maiatoday.rhymes.wishes
+import kotlin.time.Duration.Companion.milliseconds
+
+val  basketFlow: Flow<String> = channelFlow {
+    launch {
+        for (line in basket) {
+            delay(100.milliseconds)
+            send(line)
+        }
+    }
+}
+
+val wishFlow = flow {
+    for (wish in wishes) {
+        emit(wish)
+    }
+}
+
+fun main() = runBlocking {
+    basketFlow.collect {
+        println(it)
+    }
+    wishFlow.collect {
+        println(it)
+    }
+    println(">>>> collect the wishes again")
+    wishFlow.collect {
+        println(it)
+    }
 }
